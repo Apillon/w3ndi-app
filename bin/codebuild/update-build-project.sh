51c29ca1ae286a477d1f058d4737774c89e3a7ba
Luka Golinar <luka.golinar@kalmia.si>
2023-07-14 15:21:08 +0200

    feat: config change

--- bin/codebuild/update-build-project.sh
+++ bin/codebuild/update-build-project.sh
@@ -1,3 +1,3 @@
 aws codebuild update-project --cli-input-json file://config-dev.json --profile apillon
-aws codebuild update-project --cli-input-json file://config-staging.json --profile apillon
-aws codebuild update-project --cli-input-json file://config-production.json --profile apillon
+#aws codebuild update-project --cli-input-json file://config-staging.json --profile apillon
+#aws codebuild update-project --cli-input-json file://config-production.json --profile apillon
