f20385ef000f6fd19a2aecd68ef48eefc491978d
Luka Golinar <luka.golinar@kalmia.si>
2023-07-14 12:27:29 +0200

    feat: Source change

--- bin/codebuild/create-build-project.sh
+++ bin/codebuild/create-build-project.sh
@@ -1,8 +1,8 @@
 aws codebuild create-project --cli-input-json file://config-dev.json --profile apillon
 aws codebuild create-webhook --cli-input-json file://webhook-dev.json --profile apillon
 
-aws codebuild create-project --cli-input-json file://config-staging.json --profile apillon
-aws codebuild create-webhook --cli-input-json file://webhook-staging.json --profile apillon
+# aws codebuild create-project --cli-input-json file://config-staging.json --profile apillon
+# aws codebuild create-webhook --cli-input-json file://webhook-staging.json --profile apillon
 
-aws codebuild create-project --cli-input-json file://config-production.json --profile apillon
-aws codebuild create-webhook --cli-input-json file://webhook-production.json --profile apillon
+# aws codebuild create-project --cli-input-json file://config-production.json --profile apillon
+# aws codebuild create-webhook --cli-input-json file://webhook-production.json --profile apillon
