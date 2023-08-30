54a148c4eaf1a84abf35559c612f68718edd2702
Urban Kovač <urban.kovac@kalmia.si>
2023-08-30 12:56:05 +0200

    Littie animation

---
+++ src/pages/lottie.vue
@@ -0,0 +1,37 @@
+<template>
+  Lottie:
+  <lottie-player
+    src="/assets/apillon.lottie.json"
+    background="#FFFFFF"
+    speed="1"
+    loop
+    controls
+    autoplay
+    direction="1"
+    mode="normal"
+  >
+  </lottie-player>
+  Video:
+  <video
+    class="h-full w-full transform-gpu object-contain"
+    :src="NovaWalletVideo"
+    :autoplay="true"
+    preload="auto"
+    playsinline
+    muted
+    loop
+  ></video>
+</template>
+
+<script lang="ts" setup>
+import NovaWalletVideo from '~/assets/video/apillon test.webm';
+useHead({
+  script: [
+    {
+      src: 'https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js',
+      type: 'text/javascript',
+      async: true,
+    },
+  ],
+});
+</script>
